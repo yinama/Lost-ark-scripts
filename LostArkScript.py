b6179611dcf4347e239f34d15d806e2af17095db
Ying Ma <mayingjunlang95@163.com>
2022-04-13 09:46:56 -0400

    Add 2s sleep

--- LostArkScript.py
+++ LostArkScript.py
@@ -1,4 +1,5 @@
-import PySimpleGUI as sg                      
+import PySimpleGUI as sg             
+import time         
 from automine import autoPressG,autoMine,autoLog,autoFish
 
 layout = [[sg.Button('Auto Press G',size=(12,2))],
@@ -9,12 +10,13 @@ layout = [[sg.Button('Auto Press G',size=(12,2))],
 window = sg.Window('Lost Ark Script', layout,size=(600, 400)) 
 
 event, values = window.read()             
-
-if event == 'Auto Press G':
-    autoPressG()
-elif event == 'Auto Mine':
-    autoMine()
-elif event == 'Auto Mine':
-    autoLog()
-elif event == 'Auto Mine':
-    autoFish()        
+if event:
+    time.sleep(2)
+    if event == 'Auto Press G':
+        autoPressG()
+    elif event == 'Auto Mine':
+        autoMine()
+    elif event == 'Auto Logging':
+        autoLog()
+    elif event == 'Auto Fishing':
+        autoFish()  
